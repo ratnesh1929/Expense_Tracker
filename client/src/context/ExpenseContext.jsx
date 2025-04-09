794df3395b34cfa4b7b939b12772d6f416c7bd38
ratnesh1929 <ratneshmishra1160@gmail.com>
2025-04-09 10:11:31 +0530

    fix the import

--- client/src/context/ExpenseContext.jsx
+++ client/src/context/ExpenseContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import API from '../utils/api';
+import API from '../utils/API'; 
 
 const ExpenseContext = createContext();
 
