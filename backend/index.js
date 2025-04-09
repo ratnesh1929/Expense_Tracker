8b67a594f293bbd8df47542b644f353bf544d96d
ratnesh1929 <ratneshmishra1160@gmail.com>
2025-04-09 10:33:55 +0530

    fix issue

--- backend/index.js
+++ backend/index.js
@@ -27,5 +27,10 @@ const expenseRoutes = require('./src/routes/expenseRoutes');
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
